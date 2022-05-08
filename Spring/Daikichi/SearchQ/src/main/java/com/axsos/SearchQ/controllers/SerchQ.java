e249f300c74828c905abee4bb22b00586db2fa72
ALBIDAQ <hana.albidaq@enabel.be>
2022-05-08 14:38:08 +0300

    Sercq

---
+++ Spring/Daikichi/SearchQ/src/main/java/com/axsos/SearchQ/controllers/SerchQ.java
@@ -0,0 +1,21 @@
+package com.axsos.SearchQ.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SerchQ {
+	@RequestMapping("/")
+    public String index(@RequestParam(value="name", required=false) String searchQuery,@RequestParam(value="l_name", required=false) String lastName ,@RequestParam(value="times", required=false) int times){
+		if (searchQuery==null) {
+			return "Hello Human";
+		}
+		else {
+		
+        return "Hello Human: " + searchQuery +lastName;
+    }
+	
+	}
+}
+	
---
+++ Spring/Daikichi/SearchQ/src/test/java/com/axsos/SearchQ/SearchQApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axsos.SearchQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchQApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
