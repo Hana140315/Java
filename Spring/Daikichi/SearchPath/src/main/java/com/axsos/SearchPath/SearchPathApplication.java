f988acb0a5dc987bbd40e512b55477457d5911e5
ALBIDAQ <hana.albidaq@enabel.be>
2022-05-08 15:33:11 +0300

    SerchPath

---
+++ Spring/Daikichi/SearchPath/src/main/java/com/axsos/SearchPath/SearchPathApplication.java
@@ -0,0 +1,13 @@
+package com.axsos.SearchPath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchPathApplication.class, args);
+	}
+
+}
---
+++ Spring/Daikichi/SearchPath/src/test/java/com/axsos/SearchPath/SearchPathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axsos.SearchPath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchPathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
